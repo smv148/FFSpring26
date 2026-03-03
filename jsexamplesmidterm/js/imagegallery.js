6a5a2c7ee5900fd44b7f8f1a625be742a739b3f2
smv148 <smv148@miami.edu>
2026-03-03 23:17:26 +0000

    march 3rd complete

--- jsexamplesmidterm/js/imagegallery.js
+++ jsexamplesmidterm/js/imagegallery.js
@@ -2,3 +2,70 @@
 
 let galleryClass = document.querySelectorAll(".imageGallery");
 console.log(galleryClass);
+console.log(galleryClass.length);
+
+
+galleryClass[0].style.display = "none";
+galleryClass[1].style.display = "none";
+galleryClass[2].style.display = "none";
+
+/*
+for(let i = 0;i<10'i++){
+    console.log(i);
+}
+*/
+
+for(let i = 0;i<galleryClass.length;i++){
+    galleryClass[i].style.display = "none";
+}
+
+let currentImage = 0;
+
+galleryClass[currentImage].style.display = "block";
+
+let nextButton = document.getElementById("next");
+
+nextButton.addEventListener("click", function(){
+    galleryClass[currentImage].style.display = "none";
+    currentImage = currentImage + 1;
+    if(currentImage == galleryClass.length){
+        currentImage = 0;
+    }
+    galleryClass[currentImage].style.display = "block";
+
+});
+
+let prevButton = document.getElementById("previous");
+
+prevButton.addEventListener("click", function(){
+    galleryClass[currentImage].style.display = "none";
+    currentImage = currentImage - 1;
+    if(currentImage == -1){
+        currentImage = galleryClass.length -1;
+    }
+    galleryClass[currentImage].style.display = "block";
+})
+
+let startButton = document.getElementById("start");
+
+let autoCycleActive = false;
+let cycleInterval;
+
+startButton.addEventListener("click", function(){
+    if(autoCycleActive == false){
+        autoCycleActive = true;
+        cycleInterval= setInterval(function(){
+            galleryClass[currentIndex].style.display = "none";
+        }
+})
+
+
+         cycleInterval = setInterval(function() {
+            galleryClass[currentIndex].style.display = "none";
+            currentIndex++;
+            if (currentIndex >= galleryClass.length) {
+                currentIndex = 0;
+            }
+            galleryClass[currentIndex].style.display = "block";
+        }, 3000);
+        autoCycleActive = true;
